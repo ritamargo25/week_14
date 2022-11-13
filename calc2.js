c52ff61292244b628a9884af88574b0f4bbff62b
ritamargo25 <daisypearl@mail.ru>
2022-11-14 00:01:51 +0300

    innit commit

---
+++ calc2.js
@@ -0,0 +1,27 @@
+let act;
+function func() {
+    let result;
+    let number1 = (document.getElementById('number1').value);
+    let number2 = (document.getElementById('number2').value);
+    switch (act) {
+        case '+':
+            result = +number1 + +number2;
+            break;
+    }
+    switch (act) {
+        case '-':
+            result = number1 - number2;
+            break;
+    }
+    switch (act) {
+        case '*':
+            result = number1 * number2;
+            break;
+    }
+    switch (act) {
+        case '/':
+            result = number1 / number2;
+            break;
+    }
+    document.getElementById("result").innerHTML = result;
+}
---
+++ gallery.js
@@ -0,0 +1,4 @@
+function changeMe() {
+    let baliImage = document.getElementById('baliImage');
+    baliImage.src = "https://images.unsplash.com/photo-1559305289-4c31700ba9cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80";
+}
